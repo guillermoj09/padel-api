4acadc1b5f90ce4d38082ea3aab9f41deb8c5c77
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-05-31 23:17:06 -0400

    to prod

--- src/auth/application/use-cases/login.use-case.ts
+++ src/auth/application/use-cases/login.use-case.ts
@@ -1,7 +1,7 @@
 import { UsersReaderPort } from '../../domain/ports/users-reader.port';
 import { TokenSignerPort } from '../../domain/ports/token-signer.port';
 import { PasswordService } from '../services/password.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class LoginUseCase {
@@ -9,19 +9,30 @@ export class LoginUseCase {
     private readonly usersReader: UsersReaderPort,
     private readonly tokenSigner: TokenSignerPort,
     private readonly passwordService: PasswordService,
-  ) {}
+  ) { }
 
   async execute(input: { email: string; password: string }) {
     const user = await this.usersReader.findByEmail(input.email);
-    if (!user) throw new Error('Credenciales inválidas');
+
+    if (!user) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
 
     const ok = await this.passwordService.compare(input.password, user.password);
-    if (!ok) throw new Error('Credenciales inválidas');
 
-    const role = String(user.type).toLowerCase(); // 'administrador' | 'cliente'
+    if (!ok) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
+
+    const role = String(user.type).toLowerCase();
 
     const token = await this.tokenSigner.sign(
-      { sub: user.id, email: user.email, type: role, v: user.tokenVersion as any },
+      {
+        sub: user.id,
+        email: user.email,
+        type: role,
+        v: user.tokenVersion as any,
+      },
       '7d',
     );
 
